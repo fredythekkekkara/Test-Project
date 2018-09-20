96bce2b1365325f0a745ff36b986c7fc50bcec34
fredythekkekkara <32589978+fredythekkekkara@users.noreply.github.com>
2018-09-20 11:56:43 +0530

    Add files via upload

---
+++ Test_ProjectGit/Test_ProjectGit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Test_ProjectGit
+//
+//  Created by IRIS Medical Solutions on 20/09/18.
+//  Copyright © 2018 IRIS Medical Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
